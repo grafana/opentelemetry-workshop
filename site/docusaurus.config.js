322e5445da9a09c1df86cc69ca06536edc4f6c8f
Tom Donohue <monodot@gmail.com>
2024-08-27 17:42:39 +0100

    Remove intro link for now

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -94,15 +94,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
